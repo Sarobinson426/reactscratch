799851fc408d609bbd5898ce457e310ff65f443b
Shaun Robinson <Sarobinson426@gmail.com>
2021-07-25 11:48:40 -0700

    Added in the .babelrc for the transformations in code. .gitignore for node_modules added in App.css and App.js. In App.js is the main file... call App extends Component { render() }. These are where you import stateless functions and what not to create your pages/site.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import { hot } from "react-hot-loader";
+
+class App extends Component {
+    render() {
+        return(
+            <div className="App">
+                <h1>It's a bad man's world!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
